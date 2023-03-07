03afbc69a33180cca86245201d4b14adf4bc2522
Ali01 <alirahanet@gmail.com>
2023-03-07 20:26:43 +0430

    linters error solved

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-import {Book} from './modules/book_class.js'
-import { saveLocal } from 
-"./modules/localStorage_Functions.js";
+import { Book } from './modules/book_class.js';
+import { saveLocal } from
+'./modules/localStorage_Functions.js';
 import {
-    addBtn,create,collection,createList,clearPlaceHolder
-} from './modules/test.js'
+  addBtn, create, collection, createList, clearPlaceHolder,
+} from './modules/test.js';
 
 import {
-    links,browse
-} from './modules/show_hide.js'
+  links, browse,
+} from './modules/show_hide.js';
 
 import { timeDate } from './modules/date_time.js';
 
 window.onload = () => {
-    const start = new Book();
-    setInterval(timeDate,1000);
-    addBtn.addEventListener('click',()=>{
-        create()
-        saveLocal(collection)
-        clearPlaceHolder()
-    })
+  const start = new Book();
+  setInterval(timeDate, 1000);
+  addBtn.addEventListener('click', () => {
+    create();
+    saveLocal(collection);
+    clearPlaceHolder();
+  });
 
-    collection.forEach(element => {
-        createList(element)
+  collection.forEach((element) => {
+    createList(element);
+  });
+
+  links.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      browse(e);
     });
-
-    links.forEach((link)=>{
-        link.addEventListener('click',(e)=>{
-            browse(e)
-        })
-    })
-
-}
+  });
+};
